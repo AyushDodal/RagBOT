b29b860a75e8bcf1fcfb049f875a575dc21e90ef
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-23 12:18:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,29 @@ st.set_page_config(
   page_icon = "📚 🔎 🤖 💬"
 )
 
-st.write("Hello!")
+st.write("Hello!👋", \n, "This is a Retrieval Augmented Generation Bot - RAG-BOT.", \n, "You can upload files, they will get stored in a Vector Database for the LLM to refer." )
+
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
