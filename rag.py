b5cee6553139e48aeba58345f1013c323a1789d7
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-23 13:35:39 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,6 +1,6 @@
 from llama_index.core.node_parser import SentenceSplitter
 
-def chunkify(file: List):
+def chunkify(file):
   splitter = SentenceSplitter(chunk_size=1024)
   nodes = splitter.get_nodes_from_documents(documents)
   return nodes
