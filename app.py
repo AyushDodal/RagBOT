54aa71bbbfbbd2cb978ec1aa9c0ac2d5ba1b77d9
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-27 12:18:01 +0530

    Remove unused cv2 import from app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from pypdf import PdfReader
-import cv2
+
 from sentence_splitter import SentenceSplitter
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
