5eef8027e411b127f15cf1bdb2db5e8d49be71b3
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-27 10:01:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-'''from sentence_splitter import SentenceSplitter
+from sentence_splitter import SentenceSplitter
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
@@ -91,18 +91,18 @@ def main():
         # Display user message in chat message container
         st.chat_message("user").markdown(user_prompt)
         # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": user_prompt})'''
+        st.session_state.messages.append({"role": "user", "content": user_prompt})
 
-        '''if prompt.files:
+        if prompt.files:
             for upload_file in files:
                 file_paths = _get_file_path(upload_file)
                 text = load_documents(file_paths)
                 chunks = chunkify(text)
                 vectorstores = get_vectorstore(chunks)
-                assistant_reply = rag_chain(vectorstores, user_prompt)'''
+                assistant_reply = rag_chain(vectorstores, user_prompt)
 
         
-        '''response = f"Echo: {prompt.text}"
+        response = f"Echo: {prompt.text}"
         # Display assistant response in chat message container
         with st.chat_message("assistant"):
             st.markdown(response)
@@ -115,11 +115,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()'''
+    main()
 
 
 
-import streamlit as st
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
