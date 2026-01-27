6cd81c35fa911f5dea5c13c5b77ea9ddb5e1579e
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-27 09:53:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-# type: ignore
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai.embeddings import OpenAIEmbeddings
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_community.vectorstores import FAISS 
-from langchain_openai.chat_models import ChatOpenAI
-from unstructured.partition.auto import partition
-from langchain import hub
-from dotenv import load_dotenv
-import streamlit as st
-import logging
-import os
-load_dotenv()
+from sentence_splitter import SentenceSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 
-
-prompt = hub.pull("rlm/rag-prompt")
 llm = ChatOpenAI(temperature=0.6, model="gpt-4o-mini")
+
+
+# Generate temporary file path of uploaded docs
+def _get_file_path(file_upload):
+
+    temp_dir = "temp"
+    os.makedirs(temp_dir, exist_ok=True)  # Ensure the directory exists
+
+    if isinstance(file_upload, str):
+        file_path = file_upload  
+    else:
+        file_path = os.path.join(temp_dir, file_upload.name)
+        with open(file_path, "wb") as f:
+            f.write(file_upload.getbuffer())
+        return file_path
 
 
 # Load and extract text from one or multiple PDF/docx/pptx/txt files.
@@ -31,17 +34,12 @@ def load_documents(file_paths):
     return "\n\n".join(all_text)
 
 
-# Split a long text into smaller chunks, uses token-based splitting.
-def split_text(text: str):
-    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-    chunk_size=1000,
-    chunk_overlap=300,
-    )
-    print(text_splitter)
-    return text_splitter.split_text(text)
+def chunkify(text):
+    splitter = SentenceSplitter(chunk_size=1024)
+    nodes = splitter.get_nodes_from_documents(text)
+    return nodes
 
-
-# create embeddings and load chunks to vector stores
+    
 def get_vectorstore(chunks):
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
@@ -68,78 +66,53 @@ def rag_chain(vectorstore, question):
     return qa_chain.invoke(question)
 
 
-# Generate temporary file path of uploaded docs
-def _get_file_path(file_upload):
-
-    temp_dir = "temp"
-    os.makedirs(temp_dir, exist_ok=True)  # Ensure the directory exists
-
-    if isinstance(file_upload, str):
-        file_path = file_upload  
-    else:
-        file_path = os.path.join(temp_dir, file_upload.name)
-        with open(file_path, "wb") as f:
-            f.write(file_upload.getbuffer())
-        return file_path
 
 
-# Main Streamlit app function
+
+
 def main():
-    st.title("Chat with Multiple Documents(pdf, docx, ppt, txt)")
-    logging.info("App started")
+    import streamlit as st
 
-    if 'messages' not in st.session_state:
-        st.session_state.messages = [
-            {
-                "role": "assistant",
-                "content": (
-                    "Hi there! How can I help you today?")
-            }
-        ]
-
-
- 
-    file_upload = st.sidebar.file_uploader(
-    label="Upload", type=["pdf", "docx", "pptx","txt"], 
-    accept_multiple_files=True,
-    key="pdf_uploader"
-    )
-
-    if file_upload:     
-        st.success("File uploaded successfully! You can now ask your question.")
-
-
-
-    # Display existing messages
-    for msg in st.session_state.messages:
-        with st.chat_message(msg["role"]):
-            st.markdown(msg["content"])
+    st.title("Echo Bot")
     
-    user_prompt = st.chat_input("Your question")
-
-    # For user message
-    if user_prompt:
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    
+    # React to user input
+    prompt = st.chat_input("What is up?", accept_file=True, accept_audio=True, file_type=["pdf", "jpg", "jpeg", "png"])
+    if prompt.text:
+        user_prompt = prompt.text
+        # Display user message in chat message container
+        st.chat_message("user").markdown(user_prompt)
+        # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": user_prompt})
-        with st.chat_message("user"):
-            st.markdown(user_prompt)
 
-        # Stream assistant response
-        with st.chat_message("assistant"):
-            logging.info("Generating response...")
-            with st.spinner("Processing..."): 
-                
- 
-                file_paths = [_get_file_path(f) for f in file_upload]
+        if prompt.files:
+            for upload_file in files:
+                file_paths = _get_file_path(upload_file)
                 text = load_documents(file_paths)
-                chunked_text = split_text(text)
-                vectorstores = get_vectorstore(chunked_text)
+                chunks = chunkify(text)
+                vectorstores = get_vectorstore(chunks)
                 assistant_reply = rag_chain(vectorstores, user_prompt)
 
-                st.session_state.messages.append({"role": "assistant", "content": assistant_reply})
-                st.markdown(assistant_reply)
+        
+        response = f"Echo: {prompt.text}"
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": assistant_reply})
 
 
 
 
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
