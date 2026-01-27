bb740dad5a053ad593a76d1a1d8da8f5b234efb1
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-27 12:30:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def load_documents(file_paths):
 
 
 def chunkify(text):
-    splitter = SentenceSplitter(max_length=1024)
+    splitter = SentenceSplitter(1024)
     nodes = splitter.get_nodes_from_documents(text)
     return nodes
 
