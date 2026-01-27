e4ca1837b8d51a8370b69129cca964282088386a
Ayush Dodal <68580372+AyushDodal@users.noreply.github.com>
2026-01-27 12:45:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def load_documents(file_paths):
 def chunkify(text):
     if not isinstance(text, str):
         text = "\n".join(text)
-    splitter = SentenceSplitter()
+    splitter = SentenceSplitter("en")
     nodes = splitter.get_nodes_from_documents(text)
     return nodes
 
